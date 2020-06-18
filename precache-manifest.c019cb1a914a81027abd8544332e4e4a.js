5d62fe147a69984d461cf6688bd07df270af0e76
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-06-18 14:18:13 +0200

    Updates

---
+++ precache-manifest.c019cb1a914a81027abd8544332e4e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a680b2549f831fee2f1380ddaf7795b8",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "b757d44f9fffac225f48",
+    "url": "/react-todo-list/static/css/main.616818a6.chunk.css"
+  },
+  {
+    "revision": "9fa76d9e6c2f9f65aa70",
+    "url": "/react-todo-list/static/js/2.4674c968.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.4674c968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b757d44f9fffac225f48",
+    "url": "/react-todo-list/static/js/main.883f6d3c.chunk.js"
+  },
+  {
+    "revision": "6fe1acc0ae913419c7ae",
+    "url": "/react-todo-list/static/js/runtime-main.ee912ae9.js"
+  }
+]);
